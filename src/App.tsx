40579e44e67eb56497c516652be9bc17bcf1e544
geobot <stroinsky.a@gmail.com>
2024-01-05 17:11:41 +0200

    2xl width adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <section className="p-5 flex gap-5 flex-col bg-gray-800 min-h-full md:items-center">
 
-            <div className="md:w-1/2 xl:w-1/3 flex flex-col gap-5">
+            <div className="md:w-1/2 xl:w-1/3 2xl:w-1/4 flex flex-col gap-5">
                 <WeatherWidget/>
                 <TodoWidget/>
             </div>
