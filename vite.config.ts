f0840dc313e0d102d1bb800075821128d6c9c834
geobot <stroinsky.a@gmail.com>
2024-01-05 17:10:52 +0200

    remove https

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
 import react            from '@vitejs/plugin-react-swc'
 import tsPaths          from 'vite-tsconfig-paths'
-import mkcert           from 'vite-plugin-mkcert'
 
 
 // https://vitejs.dev/config/
 export default defineConfig( {
     plugins: [
-        mkcert(),
         tsPaths(),
         react(),
     ],
     server: {
-        https: true,
+        // https: true,
         port: 10000,
         watch: {
             ignored: [ '/.idea/**', '/src/*.d.ts', '/*.d.ts' ]
